11a7171e4081d8598e6f2fe8acc2ff1fc5fd7966
KihaJeong1994 <jkha7371@gmail.com>
2023-06-27 17:43:21 +0900

    web socket stomp : add interceptor for logging

---
+++ src/main/java/com/example/springwebmvctemplate/common/websocket/LoggingInterceptor.java
@@ -0,0 +1,25 @@
+package com.example.springwebmvctemplate.common.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements ChannelInterceptor {
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+        String sessionId = headerAccessor.getSessionId();
+        if(StompCommand.CONNECT.equals(headerAccessor.getCommand())){
+            log.info("######### " + sessionId + " is connected successfully");
+        }
+        else if(StompCommand.DISCONNECT.equals(headerAccessor.getCommand())){
+            log.info("######### " + sessionId + " is disconnected successfully");
+        }
+    }
+}
--- src/main/java/com/example/springwebmvctemplate/common/websocket/WebSocketConfig.java
+++ src/main/java/com/example/springwebmvctemplate/common/websocket/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.example.springwebmvctemplate.common.websocket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -22,5 +26,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.setApplicationDestinationPrefixes("/app"); // STOMP messages whose destination header begins with /app are routed to @MessageMapping methods in @Controller
         config.setUserDestinationPrefix("/user");
         config.enableSimpleBroker("/topic", "/queue"); // use the built-in message broker for subscriptions and broadcasting whose destination header begins with /topic , /queue
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(loggingInterceptor);
     }
 }
